0d042edcfa2b38e2e9a80d5e5af024faee9081b7
Diego Orellana <20245281@esen.edu.sv>
2026-09-04 12:15:06 -0600

    chore: proyecto inicial dart create

---
+++ ejercicio_2/bin/ejercicio_2.dart
@@ -0,0 +1,5 @@
+import 'package:ejercicio_2/ejercicio_2.dart' as ejercicio_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ejercicio_2.calculate()}!');
+}
---
+++ ejercicio_2/test/ejercicio_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:ejercicio_2/ejercicio_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
